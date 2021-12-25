32be12f3b5e6b0630e0788a6e916713ff3a343b5
hari-ls <hari@logicsync.com.au>
2021-12-25 15:36:16 +1030

    Updated script to incorporate DOM manipulation for the quiz steps: start, present & complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,15 +25,78 @@ var start = document.querySelector("#start-quiz");
 var submit = document.querySelector("#submit");
 var clear = document.querySelector("#clear");
 var goBack = document.querySelector("#back");
+var query = document.querySelector(".query");
+var selection = document.querySelector(".selection");
+var outcome = document.querySelector(".outcome");
 
 // Initialise operators
-var leaderBoard;
+var leaderBoard,
+  question,
+  options,
+  listItem,
+  totalQuestions = questionBank.length;
 
-// On start - present questions
+// On start - present questions and options
+var startQuiz = start.addEventListener("click", function (event) {
+  event.preventDefault();
+  welcome.setAttribute("style", "display: none");
+  quiz.setAttribute("style", "display: block");
+  presentQuestion(questionBank[0]);
+  presentOptions(questionBank[0]);
+  presentOutcome(false);
+});
 
+var presentQuestion = function (obj) {
+  question = obj.question;
+  query.textContent = question;
+};
+
+var presentOptions = function (obj) {
+  options = obj.options;
+  selection.innerHTML = "";
+  for (let i = 0; i < options.length; i++) {
+    listItem = document.createElement("li");
+    listItem.setAttribute("class", "options");
+    listItem.setAttribute("data-option", i);
+    listItem.textContent = options[i];
+    selection.appendChild(listItem);
+  }
+};
+
+var presentOutcome = function (bool) {
+  outcome.setAttribute("style", "display: block");
+  if (bool) {
+    outcome.textContent = "Correct!";
+  } else {
+    outcome.textContent = "Wrong!";
+  }
+  setInterval(function () {
+    outcome.setAttribute("style", "display: none");
+  }, 1000);
+  clearInterval();
+};
+
+var quizComplete = function () {
+  quiz.setAttribute("style", "display: none");
+  result.setAttribute("style", "display: block");
+};
+
+// presentQuestion(questionBank[0]);
+// presentOptions(questionBank[0]);
+// presentOutcome(false);
+
+// setInterval(function () {
+//   presentQuestion(questionBank[1]);
+// }, 5000);
+// setInterval(function () {
+//   presentQuestion(questionBank[2]);
+// }, 10000);
+// setInterval(function () {
+//   quizComplete();
+// }, 10000);
 // List for a selection
 
-// Check correct or incorrect
+// Check correct or incorrect and present outcome
 
 // Present next - until last
 
