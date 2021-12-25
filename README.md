8065b49933fca4beb1f8bba5e0594f0d0506fe85
hari-ls <hari@logicsync.com.au>
2021-12-25 14:28:38 +1030

    Added elements, basic styles and initilised values in the script

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Code Quiz
+# Coding Quiz
 
 Design a coding quiz as per the provided User Story, Acceptance Criteria and Mockup (as reference).
---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+// Initialise values
+var questionBank = [
+  {
+    question: "First question",
+    options: ["one", "two", "three", "four"],
+    answer: 0,
+  },
+  {
+    question: "Second question",
+    options: ["one", "two", "three", "four"],
+    answer: 0,
+  },
+  {
+    question: "Third question",
+    options: ["one", "two", "three", "four"],
+    answer: 0,
+  },
+];
+
+// Initialise selectors
+var welcome = document.querySelector(".welcome");
+var quiz = document.querySelector(".quiz");
+var result = document.querySelector(".result");
+var start = document.querySelector("#start-quiz");
+var submit = document.querySelector("#submit");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#back");
+
+// Initialise operators
+var leaderBoard;
+
+// On start - present questions
+
+// List for a selection
+
+// Check correct or incorrect
+
+// Present next - until last
+
+// Calculate and present result
+
+// Update leaderboard
